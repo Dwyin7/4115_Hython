c1d4324948cd70ca080b91e4eeaf86195eaf02b5
Dwyin7 <50932467+davidyinbu@users.noreply.github.com>
2024-04-21 19:10:19 -0400

    Revert "if while and for semantic checker"
    This reverts commit 3c5855d0eeba43e8c68222041fb28fd9bf1f9e06.

--- src/semant.ml
+++ src/semant.ml
@@ -185,38 +185,10 @@ let rec check_statements scope statments sstatments =
 (* TODO: check function formals *)
 
 (* TODO: check If *)
-let check_if_statement cond_expr then_stmt else_stmt scope =
-  let cond_type, checked_cond = check_expr scope cond_expr in
-  if cond_type != P_bool then
-    raise (Failure "If statement contains non-boolean value");
-  let _, checked_then = check_statement scope then_stmt in
-  let _, checked_else = check_statement scope else_stmt in
-  P_void, SIf (checked_cond, checked_then, checked_else)
 
 (* TODO: check While *)
-let check_while_statement cond_expr loop_stmt scope =
-  let cond_type, checked_cond = check_expr scope cond_expr in
-  if cond_type != P_bool then
-    raise (Failure "While statement contains non-boolean value");
-  
-  let new_scope = create_scope (Some scope)
-  in
-  let _, checked_loop_stmt = check_statement new_scope loop_stmt in
-  P_void, SWhile (checked_cond, checked_loop_stmt)
 
 (* TODO: check For *)
-let check_for_statement iterator start_expr end_expr loop_stmt scope =
-  let start_type, checked_start = check_expr scope start_expr in
-  let end_type, checked_end = check_expr scope end_expr in
-
-  if start_type != P_int || end_type != P_int then
-    raise (Failure "For loop has to be integer ranges");
-  
-  let iter_scope = add_variable_check_dup iterator P_int scope in  
-  let new_scope = create_scope (Some iter_scope)
-  in
-  let _, checked_loop_stmt = check_statement new_scope loop_stmt in
-  P_void, SFor (iterator, checked_start, checked_end, checked_loop_stmt)
 
 (* TODO: check expr *)
 
