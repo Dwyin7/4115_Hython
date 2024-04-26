1fc3d3826b2c9c7dd055bbb1e26ecc4d73992f93
Dwyin7 <50932467+davidyinbu@users.noreply.github.com>
2024-04-26 16:26:31 -0400

    Update the func_decl

--- src/semant.ml
+++ src/semant.ml
@@ -2,7 +2,6 @@ open Ast
 open Sast
 module StringMap = Map.Make (String)
 
-
 (* TODO: Verify a list of bindings has no duplicate names  *)
 (* cannot have same var name in same scope *)
 
@@ -18,18 +17,21 @@ type symbol_table = {
 
 let string_of_symbol_table scope =
   let variables_str =
-    StringMap.fold (fun name typ acc ->
-        acc ^ name ^ " : " ^ string_of_typ typ ^ "\n"
-      ) scope.variables ""
+    StringMap.fold
+      (fun name typ acc -> acc ^ name ^ " : " ^ string_of_typ typ ^ "\n")
+      scope.variables ""
   in
   let functions_str =
-    StringMap.fold (fun name signature acc ->
+    StringMap.fold
+      (fun name signature acc ->
         let formals_str =
           List.map (fun (t, id) -> string_of_typ t ^ " " ^ id) signature.formals
           |> String.concat ", "
         in
-        acc ^ name ^ " : " ^ string_of_typ signature.ret_typ ^ " (" ^ formals_str ^ ")\n"
-      ) scope.functions ""
+        acc ^ name ^ " : "
+        ^ string_of_typ signature.ret_typ
+        ^ " (" ^ formals_str ^ ")\n")
+      scope.functions ""
   in
   "Variables:\n" ^ variables_str ^ "\nFunctions:\n" ^ functions_str
 
@@ -86,39 +88,52 @@ let rec check_expr scope expr =
       let typ = find_variable scope var in
       (typ, SId var)
   | Binop (e1, op, e2) ->
-    let _ = string_of_expr e2 in
-    let (t1, checked_e1) = check_expr scope e1 in
-    let (t2, checked_e2) = check_expr scope e2 in
-    let ret_typ = match op with
-      | Add | Sub | Mul | Div -> begin
-          match (t1, t2) with
-          | P_int, P_int -> P_int
-          | P_float, P_float -> P_float
-          | _, _ -> raise (Failure ("Invalid operands for binary operator " ^ string_of_bop op))
-        end
-      | Equal | Neq | Less | Leq | Greater | Geq -> begin
-          match (t1, t2) with
-          | P_int, P_int -> P_bool
-          | P_float, P_float -> P_bool
-          | P_char, P_char -> P_bool
-          | P_string, P_string -> P_bool
-          | P_bool, P_bool -> P_bool
-          | _, _ -> raise (Failure ("Invalid operands for binary operator " ^ string_of_bop op))
-        end
-      (* TODO: Fix And and Or *)
-      | And | Or -> begin
-          match (t1, t2) with
-          | P_bool, P_bool -> P_bool
-          | _ -> raise (Failure ("Invalid operands for binary operator " ^ string_of_bop op))
-        end
-      (* TODO: Fix Matmul after Tensor is implemented *)
-      | Matmul -> begin
-        match (t1, t2) with
-        | P_int, P_int -> P_bool
-        | _ -> raise (Failure ("Invalid operands for binary operator " ^ string_of_bop op))
-      end
-    in
-    (ret_typ, SBinop ((t1, checked_e1), op, (t2, checked_e2))) 
+      let _ = string_of_expr e2 in
+      let t1, checked_e1 = check_expr scope e1 in
+      let t2, checked_e2 = check_expr scope e2 in
+      let ret_typ =
+        match op with
+        | Add | Sub | Mul | Div -> (
+            match (t1, t2) with
+            | P_int, P_int -> P_int
+            | P_float, P_float -> P_float
+            | _, _ ->
+                raise
+                  (Failure
+                     ("Invalid operands for binary operator " ^ string_of_bop op))
+            )
+        | Equal | Neq | Less | Leq | Greater | Geq -> (
+            match (t1, t2) with
+            | P_int, P_int -> P_bool
+            | P_float, P_float -> P_bool
+            | P_char, P_char -> P_bool
+            | P_string, P_string -> P_bool
+            | P_bool, P_bool -> P_bool
+            | _, _ ->
+                raise
+                  (Failure
+                     ("Invalid operands for binary operator " ^ string_of_bop op))
+            )
+        (* TODO: Fix And and Or *)
+        | And | Or -> (
+            match (t1, t2) with
+            | P_bool, P_bool -> P_bool
+            | _ ->
+                raise
+                  (Failure
+                     ("Invalid operands for binary operator " ^ string_of_bop op))
+            )
+        (* TODO: Fix Matmul after Tensor is implemented *)
+        | Matmul -> (
+            match (t1, t2) with
+            | P_int, P_int -> P_bool
+            | _ ->
+                raise
+                  (Failure
+                     ("Invalid operands for binary operator " ^ string_of_bop op))
+            )
+      in
+      (ret_typ, SBinop ((t1, checked_e1), op, (t2, checked_e2)))
   | Call (id, exprs) ->
       (* TODO: check function formals *)
       (* TODO: Fix scope for Call and verify correct *)
@@ -137,8 +152,8 @@ let rec check_expr scope expr =
       in
       List.iter2 check_type_pairs formals_typ typs;
       (func_signature.ret_typ, SCall (id, sexprs))
-  (* TODO: Tensor *)
-  (* TODO: Lambda *)
+(* TODO: Tensor *)
+(* TODO: Lambda *)
 
 let rec check_statement (scope : symbol_table) (statement : stmt) =
   match statement with
@@ -146,14 +161,14 @@ let rec check_statement (scope : symbol_table) (statement : stmt) =
       let sexpr = check_expr scope expr in
       (scope, SExpr sexpr)
   | Block stmts ->
-    let _, checked_stmts =
-      List.fold_left
-        (fun (current_scope, checked_stmts) stmt ->
-          let new_scope, checked_stmt = check_statement current_scope stmt in
-          (new_scope, checked_stmts @ [checked_stmt])) 
-        (create_scope (Some scope), [])  
-        stmts 
-    in
+      let _, checked_stmts =
+        List.fold_left
+          (fun (current_scope, checked_stmts) stmt ->
+            let new_scope, checked_stmt = check_statement current_scope stmt in
+            (new_scope, checked_stmts @ [ checked_stmt ]))
+          (create_scope (Some scope), [])
+          stmts
+      in
       (scope, SBlock checked_stmts)
   | Bind (typ, id) ->
       if StringMap.mem id scope.variables then
@@ -182,7 +197,7 @@ let rec check_statement (scope : symbol_table) (statement : stmt) =
         in
         check_assign typ e_typ err;
         (new_scope, SBindAndAssign ((typ, id), (e_typ, sx)))
-  | Func (typ, id, binds, stmts) ->
+  | Func { ret_type = typ; fname = id; params = binds; body = stmts } ->
       if StringMap.mem id scope.functions then
         raise (Failure ("Already decalered: " ^ id ^ " , in this scope"))
       else
@@ -197,33 +212,43 @@ let rec check_statement (scope : symbol_table) (statement : stmt) =
           let _, checked_stmts =
             List.fold_left
               (fun (current_scope, checked_stmts) stmt ->
-                let new_scope, checked_stmt = check_statement current_scope stmt in
-                (new_scope, checked_stmts @ [checked_stmt]))
-              (scope_with_formals, [])  
-              stmts
+                let new_scope, checked_stmt =
+                  check_statement current_scope stmt
+                in
+                (new_scope, checked_stmts @ [ checked_stmt ]))
+              (scope_with_formals, []) stmts
           in
           checked_stmts
         in
         let new_scope = add_function id typ binds scope in
-        (new_scope, SFunc (typ, id, binds, check_function_statements))
-  | While (cond_expr, loop_stmt) -> 
+        ( new_scope,
+          SFunc
+            {
+              sret_type = typ;
+              sfname = id;
+              sparams = binds;
+              sbody = check_function_statements;
+            } )
+  | While (cond_expr, loop_stmt) ->
       let cond_type, checked_cond = check_expr scope cond_expr in
       if cond_type != P_bool then
         raise (Failure "While statement contains non-boolean value");
-      
+
       let new_scope = create_scope (Some scope) in
       let _, checked_loop_stmt = check_statement new_scope loop_stmt in
-        (scope, SWhile ((cond_type, checked_cond), checked_loop_stmt)) 
+      (scope, SWhile ((cond_type, checked_cond), checked_loop_stmt))
   | For (iterator, start_expr, loop_stmt) ->
-    let iterator_type = find_variable_current_scope scope iterator in
-    let start_type, checked_start_expr = check_expr scope start_expr in
-    if iterator_type != P_int || start_type != P_int then
-      raise (Failure "For loop has to be integer ranges");
-  
-    let loop_scope = create_scope (Some scope) in
-    let loop_scope_with_iterator = add_variable iterator P_int loop_scope in
-    let _, checked_loop_stmt = check_statement loop_scope_with_iterator loop_stmt in
-    (scope, SFor (iterator, (P_int, checked_start_expr), checked_loop_stmt))
+      let iterator_type = find_variable_current_scope scope iterator in
+      let start_type, checked_start_expr = check_expr scope start_expr in
+      if iterator_type != P_int || start_type != P_int then
+        raise (Failure "For loop has to be integer ranges");
+
+      let loop_scope = create_scope (Some scope) in
+      let loop_scope_with_iterator = add_variable iterator P_int loop_scope in
+      let _, checked_loop_stmt =
+        check_statement loop_scope_with_iterator loop_stmt
+      in
+      (scope, SFor (iterator, (P_int, checked_start_expr), checked_loop_stmt))
   (* TODO: If *)
   (* TODO: Return *)
   | x -> failwith ("Statement type not handled yet: " ^ string_of_stmt x)
@@ -248,10 +273,9 @@ let check program =
 
 (* Previous If implementation *)
 (* let check_if_statement cond_expr then_stmt else_stmt scope =
-  let cond_type, checked_cond = check_expr scope cond_expr in
-  if cond_type != P_bool then
-    raise (Failure "If statement contains non-boolean value");
-  let _, checked_then = check_statement scope then_stmt in
-  let _, checked_else = check_statement scope else_stmt in
-  P_void, SIf (checked_cond, checked_then, checked_else) *)
-
+   let cond_type, checked_cond = check_expr scope cond_expr in
+   if cond_type != P_bool then
+     raise (Failure "If statement contains non-boolean value");
+   let _, checked_then = check_statement scope then_stmt in
+   let _, checked_else = check_statement scope else_stmt in
+   P_void, SIf (checked_cond, checked_then, checked_else) *)
